f98edc7e3db2d0c055050a4c56335176ec700d31
andrxw66 <andreyshihalev099@gmail.com>
2023-02-22 16:53:46 +0300

    Desktop version done, start to do a mobile version

--- app/components/layout/Header/DialogLogin/DialogLogin.tsx
+++ app/components/layout/Header/DialogLogin/DialogLogin.tsx
@@ -1,10 +1,15 @@
-import { Htag, Input } from '@/app/components/ui'
+import { Button, Htag, Input } from '@/app/components/ui'
 import { Dialog } from '@headlessui/react'
+import cn from 'clsx'
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 import styles from './DialogLogin.module.scss'
 import { DialogProps } from './DialogLogin.props'
+import FacebookLogo from './facebook-logo.svg'
+import GoogleLogo from './google-logo.svg'
 
 export const DialogLogin = ({ isOpen, setIsOpen }: DialogProps) => {
+	const [isLogin, setIsLogin] = useState<boolean>(true)
 	function closeModal() {
 		setIsOpen(false)
 	}
@@ -31,10 +36,47 @@ export const DialogLogin = ({ isOpen, setIsOpen }: DialogProps) => {
 				<Dialog.Panel className={styles.popup}>
 					<div className={styles.popupWrapper}>
 						<Htag tag='h3' className={styles.heading}>
-							Log in to your account
+							{isLogin ? 'Log in to your account' : 'Create account'}
 						</Htag>
-						<Input />
-						<Input />
+						{isLogin && (
+							<>
+								<button className={cn(styles.oauthBtn, styles.facebookBtn)}>
+									<FacebookLogo /> Sign In with Facebook
+								</button>
+								<button className={cn(styles.oauthBtn, styles.googleBtn)}>
+									<GoogleLogo /> Sign In with Google
+								</button>
+								<span className={styles.divider}>or</span>
+							</>
+						)}
+						<form className={styles.form}>
+							<div className={styles.labelWrapper}>
+								<label className={styles.label}>E-mail</label>
+								<Input />
+							</div>
+							<div className={styles.labelWrapper}>
+								<label className={styles.label}>Password</label>
+								<Input />
+							</div>
+							<Button className={styles.authBtn}>
+								{isLogin ? 'Sign In' : 'Sign Up'}
+							</Button>
+						</form>
+						<a className={styles.recoverLink}>Forgot your password?</a>
+						<span className={styles.hr}></span>
+						<div className={styles.link}>
+							<span className={styles.linkText}>
+								{isLogin
+									? "Don't have an account?"
+									: 'Already have an account?'}
+							</span>
+							<a
+								onClick={() => setIsLogin(!isLogin)}
+								className={styles.linkChangeForm}
+							>
+								{isLogin ? 'Register' : 'Log in'}
+							</a>
+						</div>
 					</div>
 				</Dialog.Panel>
 			</motion.div>
