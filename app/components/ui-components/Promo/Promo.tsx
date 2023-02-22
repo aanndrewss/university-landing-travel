724020253b71263c4fb09b8cb6af765c1e7e5d43
andrxw66 <andreyshihalev099@gmail.com>
2023-02-22 17:54:39 +0300

    Fixed layout, start adaptive

--- app/components/ui-components/Promo/Promo.tsx
+++ app/components/ui-components/Promo/Promo.tsx
@@ -5,15 +5,17 @@ import styles from './Promo.module.scss'
 
 export const Promo: FC = () => {
 	return (
-		<section className={styles.wrapper}>
-			<Htag tag='h1'>Explore the beauty of the World</Htag>
-			<Htag tag='h4' className={styles.tag}>
-				Receive personalized recommendations for countries, hotels, activities
-				and more
-			</Htag>
-			<div className={styles.box}>
-				<span className={styles.boxText}>What would you like to do?</span>
-				<Button className={styles.boxBtn}>Start Planning</Button>
+		<section className={styles.section}>
+			<div className={styles.wrapper}>
+				<Htag tag='h1'>Explore the beauty of the World</Htag>
+				<Htag tag='h4' className={styles.tag}>
+					Receive personalized recommendations for countries, hotels, activities
+					and more
+				</Htag>
+				<div className={styles.box}>
+					<span className={styles.boxText}>What would you like to do?</span>
+					<Button className={styles.boxBtn}>Start Planning</Button>
+				</div>
 			</div>
 		</section>
 	)
